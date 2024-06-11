17536efb1c9a5c27db9d523bb509f0a059cef369
Leo Sandoval <lsandova@redhat.com>
2024-06-11 11:45:49 -0600

    include console kernel param
    Signed-off-by: Leo Sandoval <lsandova@redhat.com>

--- scripts/efi.sh
+++ scripts/efi.sh
@@ -9,9 +9,7 @@ case "$1" in
         efibootmgr -q -b $EFI_BOOTNUM -B
         ;;
     add)
-        echo -n "\\$EFI_UKI_FILE quiet $(./scripts/guess_rootfs.sh) boot=$(awk '/ \/boot / {print $1}' /etc/fstab) rd.systemd.gpt_auto=0" \
-            | iconv -f UTF8 -t UCS-2LE \
-            | efibootmgr -b $EFI_BOOTNUM -C -d /dev/vda -p 1 -L $EFI_LABEL -l $EFI_LOADER -@ - -n $EFI_BOOTNUM
+        echo -n "\\$EFI_UKI_FILE quiet console=ttyS0 $(./scripts/guess_rootfs.sh) boot=$(awk '/ \/boot / {print $1}' /etc/fstab) rd.systemd.gpt_auto=0" | iconv -f UTF8 -t UCS-2LE | efibootmgr -b $EFI_BOOTNUM -C -d /dev/vda -p 1 -L $EFI_LABEL -l $EFI_LOADER -@ - -n $EFI_BOOTNUM
         ;;
     *)
         efibootmgr
